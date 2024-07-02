b4ebb218fa701fd94ea643664a2a68745786fe9b
kch2234 <gaemkyu4@gmail.com>
2024-07-02 15:50:35 +0900

    front react upload 1

---
+++ src/components/common/LoadingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import spinner from "../../assets/imgs/spinner.svg"
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <h4>잠시 기다려주세요</h4>
+      <img
+        src={spinner}
+        alt="loading"
+      />
+    </div>
+  );
+};
+
+export default LoadingPage;
